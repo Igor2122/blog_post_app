b87e7b38caf107faa49e6b5023214701308059c4
Igor <igor_pavlenko@yahoo.com>
2018-09-11 20:58:11 +0300

    migtration to posts added

--- database/migrations/2018_09_11_090726_add_user_to_post.php
+++ database/migrations/2018_09_11_090726_add_user_to_post.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddUserIdToPosts extends Migration
+class AddUserToPost extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AddUserIdToPosts extends Migration
      */
     public function up()
     {
-        Schema::table('posts', function($table){
-            $table->interger('user_id');
+        Schema::table('posts', function (Blueprint $table) {
+            $table->integer('user_id')->nullable();     
         });
     }
 
@@ -25,8 +25,8 @@ class AddUserIdToPosts extends Migration
      */
     public function down()
     {
-        Schema::table('posts', function($table){
-            $table->dropColumn('user_id');
+        Schema::table('posts', function (Blueprint $table) {
+            //
         });
     }
 }
